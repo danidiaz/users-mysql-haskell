b408c86a7246adcb3d25e683d53aede369fe02b1
daniel <daniel@bogusemailserver.com>
2016-09-25 23:14:14 +0200

    first commit

---
+++ src/Web/Users/MySQL.hs
@@ -0,0 +1,9 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE CPP #-}
+module Web.Users.MySQL () where
+
+
