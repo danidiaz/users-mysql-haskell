d1261b49131c9e371de9af9f666aa78553141ffd
daniel <daniel@bogusemailserver.com>
2016-10-02 18:24:50 +0200

    queries

--- src/Web/Users/MySQL.hs
+++ src/Web/Users/MySQL.hs
@@ -13,6 +13,30 @@ import Database.MySQL.Base
 
 newtype Conn a = Conn { getConn :: a } deriving (Eq,Show)
 
+createUsersTable :: Query
+createUsersTable = "CREATE TABLE IF NOT EXISTS login (\
+\ lid             SERIAL UNIQUE,\
+\ created_at      TIMESTAMPTZ NOT NULL DEFAULT CURRENT_DATE,\
+\ username        VARCHAR(64)    NOT NULL UNIQUE,\
+\ password        VARCHAR(255)   NOT NULL,\
+\ email           VARCHAR(64)   NOT NULL UNIQUE,\
+\ is_active       BOOLEAN NOT NULL DEFAULT FALSE,\
+\ CONSTRAINT \"l_pk\" PRIMARY KEY (lid)\
+\ );"
+
+createUserTokenTable :: Query
+createUserTokenTable =
+    "CREATE TABLE IF NOT EXISTS login_token (\
+        \ltid             SERIAL UNIQUE,\
+        \token            UUID UNIQUE,\
+        \token_type       VARCHAR(64) NOT NULL,\
+        \lid              INTEGER NOT NULL,\
+        \created_at       TIMESTAMPTZ NOT NULL DEFAULT CURRENT_DATE,\
+        \valid_until      TIMESTAMPTZ NOT NULL,\
+        \CONSTRAINT \"lt_pk\" PRIMARY KEY (ltid),\
+        \CONSTRAINT \"lt_lid_fk\" FOREIGN KEY (lid) REFERENCES login ON DELETE CASCADE\
+    \);"
+
 instance UserStorageBackend (Conn MySQLConn) where
 
     type UserId (Conn MySQLConn) = Int64
