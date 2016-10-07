cf66a17fe94424d845e2da37b4d0408c83be389b
daniel <daniel@bogusemailserver.com>
2016-10-07 13:22:45 +0200

    env vars in code

--- tests/env.sh
+++ tests/env.sh
@@ -1,5 +1,5 @@
 export TEST_MYSQL_HOST='localhost'
 export TEST_MYSQL_PORT=3306
-export TEST_MYSQL_DATABASE=''
-export TEST_MYSQL_USER=''
-export TEST_MYSQL_PASSWORD=''
+export TEST_MYSQL_DATABASE='Redefluss'
+export TEST_MYSQL_USER='root'
+export TEST_MYSQL_PASSWORD='toor'
--- tests/tests.hs
+++ tests/tests.hs
@@ -10,6 +10,13 @@ import Web.Users.MySQL
 
 import System.Environment
 
+test_mysql_host,test_mysql_port,test_mysql_database,test_mysql_user,test_mysql_password :: String
+test_mysql_host = "TEST_MYSQL_HOST"
+test_mysql_port = "TEST_MYSQL_PORT"
+test_mysql_database = "TEST_MYSQL_DATABASE"
+test_mysql_user = "TEST_MYSQL_USER"
+test_mysql_password = "TEST_MYSQL_PASSWORD"
+
 main :: IO ()
 main = defaultMain tests
 
